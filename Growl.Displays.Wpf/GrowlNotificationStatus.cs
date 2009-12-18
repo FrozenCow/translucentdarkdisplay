30cc3659d90faf44dbd049f27a58a846f64de161
FrozenCow <frozencow@gmail.com>
2009-12-18 12:50:44 +0000

    Added numbers to GrowlNotificationStatus so that it is comparable.
    git-svn-id: https://translucentdarkdisplay.googlecode.com/svn/trunk@23 ad70dc6c-91a4-11de-ac11-cbdc8fbd58d0

--- Growl.Displays.Wpf/GrowlNotificationStatus.cs
+++ Growl.Displays.Wpf/GrowlNotificationStatus.cs
@@ -2,14 +2,14 @@ namespace Growl.Displays.Wpf
 {
     public enum GrowlNotificationStatus
     {
-        Pending,
-        Opening,
-        Opened,
-        Timingout,
-        Clicking,
-        Closing,
-        Timedout,
-        Clicked,
-        Closed
+        Pending = 0,
+        Opening = 1,
+        Opened = 2,
+        Timingout = 3,
+        Clicking = 4,
+        Closing = 5,
+        Timedout = 6,
+        Clicked = 7,
+        Closed = 8
     }
 }
